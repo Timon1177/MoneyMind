cabb9f7f5a753b8c2d98b7fab83a5cfc667e3ba3
Timon Soom <168628615+Timon1177@users.noreply.github.com>
2025-05-05 10:07:12 +0200

    fixed mainwindow.xaml

--- MoneyMind/MoneyMind/obj/Debug/net8.0-windows/expenses.g.i.cs
+++ MoneyMind/MoneyMind/obj/Debug/net8.0-windows/expenses.g.i.cs
@@ -53,7 +53,7 @@ namespace MoneyMind {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MoneyMind;V1.0.0.0;component/expenses.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MoneyMind;component/expenses.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\expenses.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
