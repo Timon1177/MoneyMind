61ef19a4c945285d01fa056b639abea50cfbf822
K4y <116443833+KayTrachsel@users.noreply.github.com>
2025-06-02 10:01:37 +0200

    bug fixes in the StockPage

--- MoneyMind/MoneyMind/StockPage.xaml.cs
+++ MoneyMind/MoneyMind/StockPage.xaml.cs
@@ -4,54 +4,66 @@ using LiveChartsCore.SkiaSharpView.WPF;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Net.Http;
 using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Globalization;
+using System.ComponentModel;
 
 namespace MoneyMind
 {
-    public partial class StockPage : Page
+    public partial class StockPage : Page, INotifyPropertyChanged
     {
-        public ISeries[] Series { get; set; }
-        public Axis[] XAxes { get; set; }
-        public Axis[] YAxes { get; set; }
-
         private readonly System.Timers.Timer _updateTimer;
-        private readonly string _apiKey;
+        private readonly string _apiKey = "18669672a9f448b2b68d537a9a11286f";
+
+        private readonly List<decimal> _priceValues;
+        private readonly List<string> _labels;
+        private readonly LineSeries<decimal> _lineSeries;
+
+        private ISeries[] _series;
+        public ISeries[] Series
+        {
+            get => _series;
+            set { _series = value; OnPropertyChanged(nameof(Series)); }
+        }
+
+        private Axis[] _xAxes;
+        public Axis[] XAxes
+        {
+            get => _xAxes;
+            set { _xAxes = value; OnPropertyChanged(nameof(XAxes)); }
+        }
+
+        private Axis[] _yAxes;
+        public Axis[] YAxes
+        {
+            get => _yAxes;
+            set { _yAxes = value; OnPropertyChanged(nameof(YAxes)); }
+        }
 
         public StockPage()
         {
             InitializeComponent();
             DataContext = this;
 
-            _apiKey = "18669672a9f448b2b68d537a9a11286f";
+            _priceValues = new List<decimal>();
+            _labels = new List<string>();
 
-            SetupChart();
-
-            _updateTimer = new System.Timers.Timer(10000); // alle 10 Sekunden
-            _updateTimer.Elapsed += async (s, e) => await Dispatcher.InvokeAsync(UpdateChart);
-            _updateTimer.Start();
-        }
-
-        private void SetupChart()
-        {
-            Series = new ISeries[]
+            _lineSeries = new LineSeries<decimal>
             {
-                new LineSeries<decimal>
-                {
-                    Values = new List<decimal>(),
-                    Fill = null
-                }
+                Values = _priceValues,
+                Fill = null
             };
+
+            Series = new ISeries[] { _lineSeries };
 
             XAxes = new Axis[]
             {
                 new Axis
                 {
-                    Labels = new List<string>(),
+                    Labels = _labels,
                     LabelsRotation = 45
                 }
             };
@@ -63,6 +75,21 @@ namespace MoneyMind
                     Labeler = value => value.ToString("C")
                 }
             };
+
+            _updateTimer = new System.Timers.Timer(15000); // alle 15 Sekunden
+            _updateTimer.Elapsed += async (s, e) => await Dispatcher.InvokeAsync(UpdateChart);
+
+            _ = LoadInitialData(); // sofort starten
+        }
+
+        private async System.Threading.Tasks.Task LoadInitialData()
+        {
+            await UpdateChart();
+
+            // "Loading..."-Text ausblenden (nur wenn vorhanden im XAML)
+            LoadingText.Visibility = Visibility.Collapsed;
+
+            _updateTimer.Start();
         }
 
         private async System.Threading.Tasks.Task UpdateChart()
@@ -70,10 +97,8 @@ namespace MoneyMind
             try
             {
                 using var client = new HttpClient();
-                string url = $"https://api.twelvedata.com/time_series?symbol=NVDA&interval=1min&outputsize=30&apikey={_apiKey}";
+                string url = $"https://api.twelvedata.com/time_series?symbol=MSFT&interval=1min&outputsize=1&apikey={_apiKey}";
                 string response = await client.GetStringAsync(url);
-
-                Console.WriteLine("API Response: " + response);
 
                 var data = JObject.Parse(response);
 
@@ -83,9 +108,6 @@ namespace MoneyMind
                     return;
                 }
 
-                var values = new List<decimal>();
-                var labels = new List<string>();
-
                 var rawValues = data["values"] as JArray;
                 if (rawValues == null || rawValues.Count == 0)
                 {
@@ -93,56 +115,32 @@ namespace MoneyMind
                     return;
                 }
 
-                foreach (var entry in rawValues)
-                {
-                    string closeStr = entry["close"]!.ToString();
-                    string timeStr = entry["datetime"]!.ToString().Substring(11); // z. B. "14:52:00"
+                var entry = rawValues.First;
+                string closeStr = entry["close"]!.ToString();
+                string timeStr = entry["datetime"]!.ToString().Substring(11);
 
-                    if (decimal.TryParse(closeStr, NumberStyles.Any, CultureInfo.InvariantCulture, out decimal close))
+                if (decimal.TryParse(closeStr, NumberStyles.Any, CultureInfo.InvariantCulture, out decimal close))
+                {
+                    _priceValues.Add(close);
+                    _labels.Add(timeStr);
+
+                    if (_priceValues.Count > 30)
                     {
-                        values.Add(close);
-                        labels.Add(timeStr);
+                        _priceValues.RemoveAt(0);
+                        _labels.RemoveAt(0);
                     }
+
+                    StockChart.InvalidateVisual();
                 }
-
-                values.Reverse();
-                labels.Reverse();
-
-                Series = new ISeries[]
-                {
-                    new LineSeries<decimal>
-                    {
-                        Values = values,
-                        Fill = null
-                    }
-                };
-
-                XAxes = new Axis[]
-                {
-                    new Axis
-                    {
-                        Labels = labels,
-                        LabelsRotation = 45
-                    }
-                };
-
-                YAxes = new Axis[]
-                {
-                    new Axis
-                    {
-                        Labeler = value => value.ToString("C")
-                    }
-                };
-
-                StockChart.Series = Series;
-                StockChart.XAxes = XAxes;
-                StockChart.YAxes = YAxes;
-                StockChart.InvalidateVisual();
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Chart update failed: " + ex.Message);
             }
         }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
